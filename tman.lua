e2b7a926ab3814cae25ae3e6076c0ef4ef58053a
Amonov Hurshed <amonov@wimark.com>
2024-02-27 17:38:16 +0300

    roach: Add function to check task ID checkid()

--- tman.lua
+++ tman.lua
@@ -50,6 +50,22 @@ function TMan.init(taskpath)
     return self
 end
 
+--- Check ID is passed and exists in database.
+-- @param id task ID
+-- @return true on success, otherwise false
+function TMan:checkid(id)
+    id = id or self.taskid.curr
+    if not id then
+        log("no current task")
+        return false
+    end
+    if not self.taskid:exist(id) then
+        log("'%s': no such task ID", id)
+        return false
+    end
+    return true
+end
+
 --- Create a new task.
 -- @param id task ID
 -- @treturn true if new task unit is created, otherwise false
@@ -73,12 +89,7 @@ end
 --- Switch to new task.
 -- @param id task ID
 function TMan:use(id)
-    if not id then
-        log("no previous task")
-        return 1
-    end
-    if not self.taskid:exist(id) then
-        log("'%s': does not exist", id)
+    if not self:checkid(id) then
         return 1
     end
     if self.taskid.curr == id then
@@ -140,47 +151,44 @@ end
 -- @param id task ID
 function TMan:show(id)
     id = id or self.taskid.curr
-    if not id then
-        log("no current task")
-        os.exit(1)
-    end
-    if not self.taskid:exist(id) then
-        log("'%s': no such task", id)
-        os.exit(1)
+    if not self:checkid(id) then
+        return 1
     end
     self.taskunit:show(id)
 end
 
 --- Amend task unit.
 -- @param id task ID
-function TMan:amend(id)
-    print("under development:", id or "no task ID")
+-- @param opt option
+function TMan:amend(id, opt)
+    id = id or self.taskid.curr
+
+    if not self:checkid(id) then
+        return 1
+    end
+    if opt == "-d" then
+        io.write("new desc: ")
+        local desc = io.read("*l")
+    else
+        log("'%s': no such option", opt)
+    end
 end
 
---- Update git branch task.
+--- Update git repos.
 function TMan:update()
-    local id = self.taskid.curr
-    if not id then
-        log("no current task")
-        return 1
-    end
-    if not self.taskid:exist(id) then
-        log("'%s': does not exist", id)
-        return 1
-    end
+    local git = gitmod.new("", "")
 
-    local git = gitmod.new(id, "")
     git:branch_default()
     git:pull(true)
     git:branch_switch()
+    return 0
 end
 
 --- Delete task.
 -- @param id task ID
 function TMan:del(id)
-    if not self.taskid:exist(id) then
-        log("'%s': no such task", id)
-        os.exit(1)
+    if not self:checkid(id) then
+        return 1
     end
 
     io.write("Do you want to continue? [Y/n] ")
@@ -203,9 +211,7 @@ end
 --- Move current task to done status.
 function TMan:done()
     local id = self.taskid.curr
-
-    if not id then
-        log("no current task")
+    if not self:checkid() then
         return 1
     end
     local git = gitmod.new(id, "develop")
@@ -233,7 +239,7 @@ function TMan:main(arg)
     elseif cmd == "getcurr" then
         self:getcurr()
     elseif cmd == "amend" then
-        self:amend()
+        self:amend(arg[2], arg[3])
     elseif cmd == "update" then
         self:update()
     elseif cmd == "del" then
