49878a05ddb12e2ae8c566d65642d2c02572015d
Amonov Hurshed <amonov@wimark.com>
2024-02-29 14:51:32 +0300

    roach: globals: Make globals more flexible

--- src/globals.lua
+++ src/globals.lua
@@ -1,4 +1,8 @@
-local homebase = "/home/roach/work/tman/"
+-- Chaneg it to where you are gonna keep all your tasks
+-- For me it is path below in home directory
+local tmanbase = "work/tman/"
+
+local homebase = os.getenv("HOME") .. "/" .. tmanbase
 G_tmanpath = homebase .. ".tman/"
 G_taskpath = homebase .. "tasks/"
 G_codebasepath = homebase .. "codebase/"
