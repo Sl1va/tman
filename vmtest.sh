ba3dcbf17223e54a78a7e4141606b34c83030db9
roach <qcockroach6@gmail.com>
2024-05-14 16:31:37 +0300

    vmtest: Add script to test on diff platforms

---
+++ vmtest.sh
@@ -0,0 +1,18 @@
+# install system deps
+sudo apt install -y lua5.1 luarocks
+luarocks install luaposix
+
+# install util
+git clone https://github.com/roachme/tman.git
+
+# make structure
+mkdir -p ~/.config/tman
+mkdir -p ~/trash/tman
+
+cat << EOF >> ~/.config/tman/config
+Install ~/tman
+Base ~/trash/tman
+EOF
+echo "source ${HOME}/tman/shell.sh" >> ~/.zshrc
+
+echo "update shell session"
