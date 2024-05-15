2e03da607d1cc430fc2134682197e5d63cfbecc9
roach <qcockroach6@gmail.com>
2024-05-16 00:20:44 +0300

    tman: Replace OOP with functional structure

--- src/tman.lua
+++ src/tman.lua
@@ -2,18 +2,12 @@
 -- Simplify workflow when working with many repos.
 -- @module TMan
 
---[[
-local HOME = os.getenv("HOME")
-local tman_path = "personal/prjs/tman/src/?.lua"
-package.path = package.path .. ";" .. HOME .. "/" .. tman_path
-]]
-
 
 -- Tman main components.
-local taskid = require("taskid")
 local struct = require("struct")
 local config = require("config")
-local taskunit = require("taskunit")
+local taskid = require("taskid").init()
+local taskunit = require("taskunit").init()
 
 -- Tman misc components.
 local gitmod = require("misc/git")
@@ -26,16 +20,18 @@ local utils = require("aux/utils")
 
 
 
-local TMan = {}
-TMan.__index = TMan
+--[[
+TODO:
+    1. Make all commands alike so they can be put in array and called
+       in main().
 
---- Class TMan
--- @type TMan
+]]
 
+-- Private functions: start --
 
 --- Check tman dir ain't corrupted and exists.
 -- @return true on success, otherwise false
-local function check_tman_struct()
+local function _check_tman_struct()
     local files = {
         config.repos,
         config.taskids,
@@ -60,22 +56,29 @@ local function check_tman_struct()
     return true
 end
 
-
-
---- Create a new instance of class TMan.
-function TMan.new()
-    local self = setmetatable({}, TMan)
-    return self
+--- Check ID is passed and exists in database.
+-- @param id task ID
+-- @return true on success, otherwise false
+local function _checkid(id)
+    id = id or taskid:getcurr()
+    if not id then
+        log:err("no current task")
+        return false
+    end
+    if not taskid:exist(id) then
+        log:err("'%s': no such task ID", id)
+        return false
+    end
+    return true
 end
 
---- Init class TMan.
-function TMan:init()
-    self.taskid = taskid.init()
-    self.taskunit = taskunit.init()
-end
+-- Private functions: end --
+
+
+-- Public functions: start --
 
 --- Init system to use a util.
-function TMan:tman_init()
+local function tman_init()
     print("init tman structure")
     -- dirs
     utils.mkdir(config.ids)
@@ -88,33 +91,12 @@ function TMan:tman_init()
     utils.touch(config.repos)
 end
 
---- Check paths and configs for proper work.
-function TMan:tman_check()
-    print("tman_check")
-end
-
---- Check ID is passed and exists in database.
--- @param id task ID
--- @return true on success, otherwise false
-function TMan:checkid(id)
-    id = id or self.taskid:getcurr()
-    if not id then
-        log:err("no current task")
-        return false
-    end
-    if not self.taskid:exist(id) then
-        log:err("'%s': no such task ID", id)
-        return false
-    end
-    return true
-end
-
 --- Add a new task.
 -- @param id task ID
 -- @param tasktype task type: bugfix, hotfix, feature. Default: bugfix
 -- @param prio task priority: lowest, low, mid, high, highest. Default: mid
 -- @treturn true if new task unit is created, otherwise false
-function TMan:add(id, tasktype, prio)
+local function tman_add(id, tasktype, prio)
     prio = prio or "mid"
     tasktype = tasktype or "bugfix"
 
@@ -122,23 +104,23 @@ function TMan:add(id, tasktype, prio)
         log:err("task ID required")
         os.exit(1)
     end
-    if not self.taskid:add(id) then
+    if not taskid:add(id) then
         log:err("'%s': already exists", id)
         os.exit(1)
     end
-    if not self.taskunit:add(id, tasktype, prio) then
+    if not taskunit:add(id, tasktype, prio) then
         log:err("could not create new task unit")
-        self.taskid:del(id)
+        taskid:del(id)
         os.exit(1)
     end
     if not struct.create(id) then
         log:err("could not create new task structure")
-        self.taskid:del(id)
-        self.taskunit:del(id)
+        taskid:del(id)
+        taskunit:del(id)
         os.exit(1)
     end
     --[[
-        TODE: this's how it should go
+        TODO: this's how it should go?
         git.create_branch()
     ]]
     return true
@@ -146,52 +128,52 @@ end
 
 --- Switch to another task.
 -- @param id task ID
-function TMan:use(id)
-    if not self:checkid(id) then
+local function use(id)
+    if not _checkid(id) then
         os.exit(1)
     end
-    if self.taskid:getcurr() == id then
+    if taskid:getcurr() == id then
         log:warning("'%s': already in use", id)
         os.exit(1)
     end
 
-    local branch = self.taskunit:getunit(id, "branch")
+    local branch = taskunit:getunit(id, "branch")
     local git = gitmod.new(id, branch)
     if not git:branch_switch(branch) then
         log:err("repo has uncommited changes")
         os.exit(1)
     end
-    self.taskid:setcurr(id)
+    taskid:setcurr(id)
     return 0
 end
 
 --- Switch to previous task.
-function TMan:prev()
-    local prev = self.taskid:getprev()
+local function tman_prev()
+    local prev = taskid:getprev()
 
     if not prev then
         log:warning("no previous task")
         os.exit(1)
     end
-    local branch = self.taskunit:getunit(prev, "branch")
+    local branch = taskunit:getunit(prev, "branch")
     local git = gitmod.new(prev, branch)
     if not git:branch_switch(branch) then
         log:err("repo has uncommited changes")
         os.exit(1)
     end
-    self.taskid:swap()
+    taskid:swap()
     return 0
 end
 
 --- Get cucrent task ID and other info.
 -- @return currentn task ID
-function TMan:_curr()
+local function _tman_curr()
     local optstring = "fi"
-    local id = self.taskid:getcurr()
+    local id = taskid:getcurr()
 
     for optopt, _, optind in getopt(arg, optstring) do
         if optopt == "f" then
-            local desc = self.taskunit:getunit(id, "desc")
+            local desc = taskunit:getunit(id, "desc")
             print(("* %-10s %s"):format(id, desc))
         elseif optopt == "i" then
             print(id or "")
@@ -205,7 +187,7 @@ end
 --- List all task IDs.
 -- Default: show only active task IDs.
 -- @param opt list option
-function TMan:list()
+local function tman_list()
     local active = true
     local completed = false
     local optstring = "Aac"
@@ -227,43 +209,43 @@ function TMan:list()
             completed = true
         end
     end
-    self.taskid:list(active, completed)
+    taskid:list(active, completed)
 end
 
 --- Show task unit metadata.
 -- @param id task ID
-function TMan:show(id)
-    id = id or self.taskid:getcurr()
+local function tman_show(id)
+    id = id or taskid:getcurr()
 
-    if not self:checkid(id) then
+    if not _checkid(id) then
         os.exit(1)
     end
-    self.taskunit:show(id)
+    taskunit:show(id)
 end
 
 --- Amend task unit.
 -- @param id task ID
 -- @param opt option
-function TMan:amend(id, opt)
-    id = id or self.taskid:getcurr()
+local function tman_amend(id, opt)
+    id = id or taskid:getcurr()
 
-    if not self:checkid(id) then
+    if not _checkid(id) then
         os.exit(1)
     end
     if opt == "-d" then
         io.write("new desc: ")
         local newdesc = io.read("*l")
-        self.taskunit:amend_desc(id, newdesc)
+        taskunit:amend_desc(id, newdesc)
     elseif opt == "-p" then
         io.write("new priority [highest|high|mid|low|lowest]: ")
         local newprio = io.read("*l")
-        self.taskunit:amend_prio(id, newprio)
+        taskunit:amend_prio(id, newprio)
     elseif opt == "-i" then
         io.write("new task ID: ")
         local newid = io.read("*l")
-        self.taskunit:amend_id(id, newid)
-        self.taskid:del(id)
-        self.taskid:add(newid)
+        taskunit:amend_id(id, newid)
+        taskid:del(id)
+        taskid:add(newid)
     elseif not opt then
         log:err("option missing")
     else
@@ -272,8 +254,8 @@ function TMan:amend(id, opt)
 end
 
 --- Create task symlinks.
-function TMan:link(id)
-    if not self:checkid(id) then
+local function tman_link(id)
+    if not _checkid(id) then
         os.exit(1)
     end
     print("under dev: gotta use struct.lua API")
@@ -282,15 +264,15 @@ end
 --- Update git repos.
 -- @param opt options
 -- roachme: It doesn't work if there is no current task
-function TMan:update(opt)
+local function tman_update(opt)
     opt = opt or "-u"
-    local id = self.taskid:getcurr()
+    local id = taskid:getcurr()
     if not id then
         log:warning("no current task")
         os.exit(1)
     end
 
-    local branch = self.taskunit:getunit(id, "branch")
+    local branch = taskunit:getunit(id, "branch")
     local git = gitmod.new(id, branch)
 
     git:branch_switch_default()
@@ -307,10 +289,10 @@ end
 
 --- Delete task.
 -- @param id task ID
-function TMan:del(id)
-    local desc = self.taskunit:getunit(id, "desc")
+local function tman_del(id)
+    local desc = taskunit:getunit(id, "desc")
 
-    if not self:checkid(id) then
+    if not _checkid(id) then
         os.exit(1)
     end
     print(("> %-8s %s"):format(id, desc))
@@ -320,8 +302,8 @@ function TMan:del(id)
         print("deletion is cancelled")
         os.exit(1)
     end
-    self.taskunit:del(id)
-    self.taskid:del(id)
+    taskunit:del(id)
+    taskid:del(id)
     struct.delete(id)
     --[[
     self.git:del(id)        -- delete task branch (need task ID from taskunit.lua)
@@ -333,14 +315,14 @@ function TMan:del(id)
 end
 
 --- Check current task and push branch for review.
-function TMan:review()
-    local id = self.taskid:getcurr()
+local function tman_review()
+    local id = taskid:getcurr()
 end
 
 --- Move current task to done status.
-function TMan:done()
-    local id = self.taskid:getcurr()
-    if not self:checkid() then
+local function tman_done()
+    local id = taskid:getcurr()
+    if not _checkid() then
         os.exit(1)
     end
     local git = gitmod.new(id, "develop")
@@ -348,19 +330,19 @@ function TMan:done()
         log:err("repo has uncommited changes")
         os.exit(1)
     end
-    self.taskid:move(self.taskid.types.COMP)
+    taskid:move(taskid.types.COMP)
 end
 
 --- Config util for your workflow
 -- @param subcmd subcommand
-function TMan:config(subcmd)
+local function tman_config(subcmd)
     if subcmd == "repo" then
         print("configure repo list")
     elseif subcmd == "" then
     end
 end
 
-function TMan:time(oper, val)
+local function tman_time(oper, val)
     local weeks = ""
     local days = ""
     local hours = ""
@@ -385,7 +367,7 @@ function TMan:time(oper, val)
 end
 
 --- Back up util configs into archive.
-function TMan:backup()
+local function tman_backup()
     -- roachme: need some tuning
     local ftar = "tman_db.tar"
     local dtar = ".tman"
@@ -403,7 +385,7 @@ function TMan:backup()
 end
 
 --- Restore util configs from archive.
-function TMan:restore()
+local function tman_restore()
     local ftar = arg[1]
 
     if not ftar then
@@ -426,58 +408,51 @@ end
 --- Interface.
 local function main()
     local cmd = arg[1] or "help"
-    local tman = TMan.new()
 
     -- posix getopt does not let permutations as GNU version
     table.remove(arg, 1)
 
-    if not check_tman_struct() and cmd ~= "init" then
+    if not _check_tman_struct() and cmd ~= "init" then
         log:err("tman structure not inited or corrupted")
         os.exit(1)
     end
 
-
-    -- system commands
     if cmd == "init" then
-        return tman:tman_init()
-    elseif cmd == "check" then
-        return tman:tman_check()
-    end
-
-    tman:init()
-    if cmd == "add" then
-        tman:add(arg[1], arg[2], arg[3])
+        return tman_init()
+    elseif cmd == "add" then
+        tman_add(arg[1], arg[2], arg[3])
     elseif cmd == "amend" then
-        tman:amend(arg[1], arg[2])
+        tman_amend(arg[1], arg[2])
     elseif cmd == "link" then
-        tman:link(arg[1])
+        tman_link(arg[1])
     elseif cmd == "use" then
-        tman:use(arg[1])
+        use(arg[1])
     elseif cmd == "show" then
-        tman:show(arg[1])
+        tman_show(arg[1])
     elseif cmd == "del" then
-        tman:del(arg[1])
+        tman_del(arg[1])
     elseif cmd == "_curr" then
-        tman:_curr()
+        _tman_curr()
     elseif cmd == "list" then
-        tman:list()
+        tman_list()
     elseif cmd == "update" then
-        tman:update(arg[1])
+        tman_update(arg[1])
     elseif cmd == "review" then
-        tman:review()
+        tman_review()
     elseif cmd == "done" then
-        tman:done()
+        tman_done()
     elseif cmd == "config" then
-        tman:config(arg[1])
+        tman_config(arg[1])
     elseif cmd == "prev" then
-        tman:prev()
+        tman_prev()
     elseif cmd == "time" then
-        tman:time(arg[1], arg[2])
+        tman_time(arg[1], arg[2])
     elseif cmd == "backup" then
         --tman:backup()
         print("under development")
     elseif cmd == "restore" then
-        tman:restore()
+        print("under development")
+        --tman_restore()
     elseif cmd == "help" then
         help.usage()
     elseif cmd == "info" then
@@ -488,5 +463,7 @@ local function main()
         log:err("'%s': no such command", cmd)
     end
 end
+
+-- Public functions: end --
 
 main()
