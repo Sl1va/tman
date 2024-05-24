1aeaac1f2415013e5911f19ded98f945bd5907bd
roach <qcockroach6@gmail.com>
2024-05-24 13:40:47 +0300

    config.lua: Place repos file to tman config

--- src/config.lua
+++ src/config.lua
@@ -7,18 +7,21 @@ local readconf = require("aux/readconf")
 
 
 local userhome = os.getenv("HOME")
+local file_tman_config = nil
+local file_tman_repos = nil
 
 -- Default places to search config file.
-local fconfig_files = {
-    "/.tman/config",
-    "/.config/tman/config",
+local tman_config_dir = {
+    "/.tman/",
+    "/.config/tman/",
 }
+
 
 --- Search config file.
 -- @return true if found, otherwise false
-local function search_config_file()
-    for _, confile in pairs(fconfig_files) do
-        confile = userhome .. "/" .. confile
+local function search_config_file(file)
+    for _, confile in pairs(tman_config_dir) do
+        confile = userhome .. "/" .. confile .. file
         if utils.access(confile) then
             return confile
         end
@@ -27,14 +30,22 @@ local function search_config_file()
 end
 
 -- TODO: where should `tman` look for config file?
-local fconfig = search_config_file()
+file_tman_config = search_config_file("config")
+file_tman_repos = search_config_file("repos")
 
-if not fconfig then
+if not file_tman_config then
     log:err("config file not found")
     os.exit(1)
 end
 
-readconf.init(fconfig)
+if not file_tman_repos then
+    log:err("repos file not found")
+    os.exit(1)
+end
+
+
+
+readconf.init(file_tman_config)
 local configvars = readconf.parse()
 
 
@@ -44,9 +55,9 @@ local _tmanbase = _base .. ".tman/"
 local _codebase = _base .. "codebase/"
 local _taskbase = _base .. "tasks/"
 local _ids = _tmanbase .. "ids/"
-
-local _repos = _tmanbase .. "repos"
 local _taskids = _tmanbase .. "taskids"
+
+local _repos = file_tman_repos
 
 return {
     -- files
