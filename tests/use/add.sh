895b161ebe0fb7c631982e50a1e8dd8a84e36f9d
roach <qcockroach6@gmail.com>
2024-05-29 17:44:39 +0300

    tests: Amend use test

--- tests/use/add.sh
+++ tests/use/add.sh
@@ -3,32 +3,29 @@
 source tman.sh
 
 TASK_PREFIX="TEST_TEST"
+ITERS=10000
 
-function tman_add_100()
+function tman_add()
 {
-    local iter_counts=100
-
-    echo "Add $iter_counts tasks"
-    for i in $(seq $iter_counts); do
+    echo "Add $ITERS tasks"
+    for i in $(seq $ITERS); do
         local taskid="${TASK_PREFIX}_$i"
         echo "My test example $taskid" | tman add $taskid
     done
 }
 
-function tman_del_100()
+function tman_del()
 {
-    local iter_counts=100
-
     echo "Add 100 tasks"
-    for i in $(seq $iter_counts); do
+    for i in $(seq $ITERS); do
         local taskid="${TASK_PREFIX}_$i"
         echo "Yes" | tman del $taskid
     done
 }
 
 
-#tman_add_100
-#tman_del_100
+#tman_add
+tman_del
 # delete: bash tests/use/add.sh  46.14s user 18.75s system 107% cpu 1:00.52 total
 # add:    bash tests/use/add.sh  39.40s user 15.31s system 107% cpu 50.929 total
 
