51e12348da36057cf08b128cd23c8709a81b8fa0
roach <qcockroach6@gmail.com>
2024-06-05 23:21:38 +0300

    aux/defaults: Add some default values

--- src/aux/defaults.lua
+++ src/aux/defaults.lua
@@ -2,7 +2,11 @@
 -- @module defaults
 
 local def_tasktypes = { "bugfix", "feature", "hotfix" }
+local def_commit_pattern = "PART: [ID] MSG"
+local def_commit_size = 50
 
 return {
     tasktypes = def_tasktypes,
+    commitpatt = def_commit_pattern,
+    commitsize = def_commit_size,
 }
