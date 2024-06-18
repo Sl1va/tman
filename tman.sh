4e2c356d00a53b78d27dc1d0d3f147c55ebcf8ce
roach <qcockroach6@gmail.com>
2024-06-18 12:32:32 +0300

    tman: Add support for set command in shell

--- tman.sh
+++ tman.sh
@@ -2,6 +2,7 @@
 
 TMAN=""
 TMAN_WD=
+TMAN_CWD=
 TMAN_BASE=
 TMAN_INSTALL=
 TMAN_TMANCONF=
@@ -41,7 +42,33 @@ function _tman_handle_command()
         wd add -q -f task
 
     elif [ "$cmd" = "set" ]; then
-        echo "WARNING:shell: set: under development"
+        # raach: Refactor it. Too complicated.
+        local myargs=("$@")
+        local myargs=("${myargs[@]:1}")
+        local flag=false
+
+        while getopts ":i" arg "${myargs[@]}";  do
+            case "$arg" in
+                i)
+                    flag=true
+                    ;;
+                *)
+                    ;;
+            esac
+        done
+
+        if [ "$flag" = true ]; then
+            taskid="$(eval "$TMAN" get curr)"
+            taskdir="${base}/${envcurr}/tasks/${taskid}"
+
+            cd "$taskdir" || return 1
+            wd add -q -f task
+
+            # switch back if CWD is not the task ID changed.
+            if [ -d "$TMAN_CWD" ]; then
+                cd "$TMAN_CWD" || return 1
+            fi
+        fi
 
     elif [ "$cmd" = "sync" ]; then
         taskid="$(eval "$TMAN" get curr)"
@@ -87,6 +114,7 @@ function tman()
     _tman_get_sys_config_vars
     _tman_form_full_command
 
+    TMAN_CWD="$(pwd)"
     eval $TMAN $@
     retcode="$?"
 
