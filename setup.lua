6f0e3d091969d7fcf7e1046d0c6bf8ccc758e785
roach <qcockroach6@gmail.com>
2024-06-19 23:49:40 +0300

    setup: Add bare setup module

---
+++ setup.lua
@@ -0,0 +1,18 @@
+
+
+--[[
+Found the logic in git project. File name setup.c:
+    1. Run this module before any command. Pro'ly all but might use a flag to
+       specify it.
+    2. Make sure tman directory is safe to perform any command. It frees the
+       rest of code logic from checks and crap like that.
+]]
+
+--[[
+
+    1. Check that every task ID has corresponding. Yeah, all. It shouldn't take
+       that much time is it seems. Tho the rest of the code'll run no error.
+       If it slows down performance, rewrite it in C. Sounds good?
+        a) unit file
+        b) task dir
+]]
