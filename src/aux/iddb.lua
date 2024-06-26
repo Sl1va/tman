caefa407857357b631ec4bf1119b43ed62409c19
roach <qcockroach6@gmail.com>
2024-06-26 16:06:14 +0300

    src/aux/iddb: Fix multiple ID database load

--- src/aux/iddb.lua
+++ src/aux/iddb.lua
@@ -23,7 +23,7 @@ end
 -- @return on success - true
 -- @return on failure - false
 local function _db_load()
-    -- roachme: make `ids = {}' empty to not load duplicates (like in unit.lua).
+    db = {} -- reset database.
     local f = io.open(idfile, "r")
 
     if not f then
