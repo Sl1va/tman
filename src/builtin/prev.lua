6fc533df1b790bc0499430946a4a5e4082400091
roach <qcockroach6@gmail.com>
2024-06-27 18:34:24 +0300

    src/builtin/prev: Delete notion of env in prev command

--- src/builtin/prev.lua
+++ src/builtin/prev.lua
@@ -8,8 +8,7 @@ local getopt = require("posix.unistd").getopt
 local function tman_prev()
     local keyhelp
     local prev = taskid.getprev()
-    local envname = arg[1]
-    local optstr = "e:h" -- roachme:API: should option be used?
+    local optstr = "h" -- roachme:API: should option be used?
     local cmdname = "prev"
 
     for optopt, _, optind in getopt(arg, optstr) do
@@ -28,15 +27,10 @@ local function tman_prev()
     end
 
     if not prev then
-        common.die(1, "no previous task\n", "")
+        common.die(1, "no previous task\n", "prev")
     end
     if not git.check(prev) then
         common.die(1, "errors in repo. Put meaningful desc here\n", "REPONAME")
-    end
-
-    if envname then
-        io.stderr:write("tman: env not supported yet. under development\n")
-        os.exit(1)
     end
 
     git.branch_switch(prev)
